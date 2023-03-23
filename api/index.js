c45cb36fb055a6af0e8a67a68369afb66c52028c
elvulturepromax <116589732+elvulturepromax@users.noreply.github.com>
2023-03-23 17:32:37 +0100

    Test with SpigotMC

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 import NextCors  from 'nextjs-cors';
 
 const apiProxy = createProxyMiddleware({
-  target: "https://github.com/elvulturepromax/proxy",
+  target: "https://www.spigotmc.org/",
   changeOrigin: true,
   pathRewrite: {
     "^/api": "" // Strip "/api" from the URL 
